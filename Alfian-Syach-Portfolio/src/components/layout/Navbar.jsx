e0416dcdc0df365f99ebbdf5b5b8ef56d8748270
Alfian Syach <127624933+alfiansyach23@users.noreply.github.com>
2026-02-08 05:53:55 +0700

    Update Navbar.jsx

--- Alfian-Syach-Portfolio/src/components/layout/Navbar.jsx
+++ Alfian-Syach-Portfolio/src/components/layout/Navbar.jsx
@@ -45,16 +45,9 @@ export default function Navbar() {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="w-6 h-6 text-blue-600"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              d="M277.8 8.6c-12.3-11.4-31.3-11.4-43.5 0l-224 208c-9.6 9-12.8 22.9-8 35.1S18.8 272 32 272l16 0 0 176c0 35.3 28.7 64 64 64l288 0c35.3 0 64-28.7 64-64l0-176 16 0c13.2 0 25-8.1 29.8-20.3s1.6-26.2-8-35.1l-224-208zM240 320l32 0c26.5 0 48 21.5 48 48l0 96-128 0 0-96c0-26.5 21.5-48 48-48z"
             />
           </svg>
         </motion.div>
@@ -143,3 +136,4 @@ export default function Navbar() {
     </motion.nav>
   );
 }
+
