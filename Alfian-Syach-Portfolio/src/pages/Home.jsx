c981c32941bb1bd6b1dc5f1e2faa547ed7a73ca0
Alfian Syach <127624933+alfiansyach23@users.noreply.github.com>
2026-02-07 19:58:41 +0700

    Update Home.jsx
    Custom folder placement for hero-vector.svg

--- Alfian-Syach-Portfolio/src/pages/Home.jsx
+++ Alfian-Syach-Portfolio/src/pages/Home.jsx
@@ -121,9 +121,10 @@ export default function Home() {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, ease: "easeInOut" }}
         >
-          <img className="h-full w-full" src="/assets/hero-vector.svg" alt="Hero Vector" />
+          <img className="h-full w-full" src="public/assets/images/hero-vector.svg" alt="Hero Vector" />
         </motion.div>
       </div>
     </div>
   );
 }
+
