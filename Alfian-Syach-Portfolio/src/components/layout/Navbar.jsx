a44e2936919d07e8962850d1bf99416913726672
Alfian Syach <127624933+alfiansyach23@users.noreply.github.com>
2026-02-08 19:48:14 +0700

    Update Navbar.jsx

--- Alfian-Syach-Portfolio/src/components/layout/Navbar.jsx
+++ Alfian-Syach-Portfolio/src/components/layout/Navbar.jsx
@@ -72,7 +72,7 @@ export default function Navbar() {
         </ul>
 
         <motion.a
-          href=""
+          href="https://drive.google.com/file/d/1vzpaS0o982Nw-wXlvWtO-sMtQ1voYA65/view?usp=drivesdk"
           className="hidden relative lg:inline-block px-4 py-2 font-medium group"
         >
           <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
@@ -120,7 +120,7 @@ export default function Navbar() {
                 </motion.li>
               ))}
               <motion.a
-                href=""
+                href="https://drive.google.com/file/d/1vzpaS0o982Nw-wXlvWtO-sMtQ1voYA65/view?usp=drivesdk"
                 className="relative inline-block px-4 py-2 font-semibold group"
                 whileHover={{ scale: 1.1 }}
               >
@@ -137,3 +137,4 @@ export default function Navbar() {
     </motion.nav>
   );
 }
+
